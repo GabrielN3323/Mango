49496d7ee5955826459f50ce3562754936550a01
Gabriel Nilton <Gabrielsoaresgodoy.rede@gmail.com>
2025-04-07 11:20:11 -0300

    crie atividade 05

---
+++ vetores(listas)/atividade05.py
@@ -0,0 +1,9 @@
+itens = []
+
+while True:
+    pedir = input('insira algo: ')
+    if pedir == 'fim':
+        break
+    else:
+        itens.append(itens)
+        print('a lista tem:',len(itens),'itens')
