1a4c5e9b49527ce0014f2e1c6a5589138e1a2cb3
Gabriel Nilton <Gabrielsoaresgodoy.rede@gmail.com>
2025-04-07 08:49:07 -0300

    crie atividade01

---
+++ vetores(listas)/atividade01.py
@@ -0,0 +1,7 @@
+cidades = "Avaré", "Bauru", "Botucatu", "Caxias do sul"
+
+cidade2 = cidades[2]
+
+cidade4 = cidades[-1]
+
+print(cidade4)
---
+++ vetores(listas)/numeroslista.py
@@ -0,0 +1,11 @@
+numeros = [10, 20, 30, 40, 50]
+
+primeiro = numeros[0] #10
+
+terceiro = numeros[2] #30
+
+ultimo = numeros[-1] #50
+
+nao = numeros[3]
+
+print()
