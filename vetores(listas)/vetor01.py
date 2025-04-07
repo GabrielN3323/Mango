c1e0430b8668ffae55a69b3c3b6d43df4a3f86d1
Gabriel Nilton <Gabrielsoaresgodoy.rede@gmail.com>
2025-04-07 07:51:00 -0300

    vetores

---
+++ vetores(listas)/vetor01.py
@@ -0,0 +1,5 @@
+##semelhante a uma fila de espera, os elementos em uma lista python tem uma ordem especifica e podem ser acessedas por sua posição
+
+lista_de_animais = ["cobra", "lobo", "gato", "onça"]
+
+print(lista_de_animais)
