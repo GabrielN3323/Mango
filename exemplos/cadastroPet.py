a3d67577d5ca0ab45f5e02befae5add8699d0ede
Gabriel Nilton <Gabrielsoaresgodoy.rede@gmail.com>
2025-04-15 15:54:25 -0300

    crie cadastro de pet

---
+++ exemplos/cadastroPet.py
@@ -0,0 +1,43 @@
+listaNomePet = []
+
+def listarPet():
+    for index,nome in enumerate(listaNomePet):
+        print(index, nome)
+
+def inserirNome(nomePet):
+
+    if nomePet != "":
+        listaNomePet.append(nomePet)
+        print(f'O pet {nomePet} foi cadastrado')
+        listarPet()
+    else:
+        print('Insira o nome do Pet')
+
+def removerPet(nomePet):
+    if nomePet in listaNomePet:
+        listaNomePet.remove(nomePet)
+        print(f'O pet {nomePet} foi removido com sucesso')
+    else:
+        print('Digite um nome valido')
+
+while True:
+
+         print('Escreva uma das opções: Adicionar nome/ Remover nome/ Listar nome ')
+
+         opções = input('escolhe uma das opções acima: ')
+
+         if opções == "Adicionar nome":
+             nomePet = input('Digite nome do pet: ')
+
+             inserirNome(nomePet)
+
+         elif opções == "Remover nome":
+
+             removerPet(nomePet)
+
+         elif opções == "listar nome":
+             listaNomePet()
+
+         else:
+             print('invalido')
+             break
